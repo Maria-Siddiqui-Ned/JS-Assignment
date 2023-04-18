d8ad76e042a126845e9a84fa6381caac446bbe85
Maria Siddiqui <mariasiddiqui_ned@yahoo.com>
2023-04-18 21:59:58 +0500

    JS Assignment 1

---
+++ Task 8 - World Cup Squad/app.js
@@ -0,0 +1,5 @@
+var WorldCupSquad = ['Ali' , 'Imran' , 'Hamza' , 'Hassan' , 'Affan' , 'Yasir' , 'Hamdan' , 'Ahmed' , 'Abu Bakr' , 'Zain' , 'Rashid' , 'Zafar' , 'Kamran' , 'Junaid' , 'Furqan'];
+var FinalTeamPlayers = WorldCupSquad.slice(0,11);
+// alert ('Pakistani Teams Players selected for WorldCup (15 Players) are \n' , alert (WorldCupSquad) );
+// alert (WorldCupSquad);
+document.write ("Final team players (11 Players) that will be playing in tomorrow match with India. \n", FinalTeamPlayers );
